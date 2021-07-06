a0cd94241edc6e7326969cf66b5e14c6c22d2d15
Himanshu Moliya <himanshuvmoliya@gmail.com>
2021-07-06 15:54:53 +0530

    RL based commercial version

--- scorecard_generation.py
+++ scorecard_generation.py
@@ -16,6 +16,10 @@ from spellchecker import SpellChecker
 
 # change path if required / for mac os just comment this line
 # pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/4.1.1/bin/tesseract'
+
+# for windows
+# pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
 import streamlit as st
 import pandas as pd
 
@@ -157,8 +161,21 @@ class Scorecard_generator:
     @st.cache(suppress_st_warning=True, allow_output_mutation=True)  
     def expensive_computation_load_data(self):
         # reading csv file from url 
-        data = pd.read_csv("all_product_data.csv")
-        print("Data Loaded")
+        # data = pd.read_csv("all_product_data.csv")
+        # print("Data Loaded")
+        try:
+            conn_db = sqlite3.connect('dynamic_database.db')
+            data = pd.read_sql_query("SELECT * FROM product_data", conn_db)
+            # print(data)
+            print("dynamic_data LOADED")
+        except Exception as e:
+            data = None
+            print("dynamic_data NOT LOADED")
+            print(e)
+            pass
+
+
+
         return data
 
     def get_product_description_from_csv(self, data, product_name):
@@ -167,7 +184,7 @@ class Scorecard_generator:
             # substring to be searched
 
             # creating and passing series to new column
-            data_index = data["Product Title"].str.find(product_name)
+            data_index = data["ProductTitle"].str.find(product_name)
 
             print("product_name", product_name)
             #blob_pname = TextBlob(product_name)
@@ -179,7 +196,7 @@ class Scorecard_generator:
 
                 data_all = data.iloc[data_index.argmax()]
 
-                text_description = data_all[['Product Title', 'Product Detail', 'Ingredients', 'Nutritional_information', 'Allergen warnings', 'Claims', 'Endorsements']].to_string(header=False, index=False)
+                text_description = data_all[['ProductTitle', 'ProductDetail', 'Ingredients', 'Nutritional_information', 'Allergenwarnings', 'Claims', 'Endorsements']].to_string(header=False, index=False)
 
                 # remove other words then text
                 text_description = re.sub('[^a-zA-Z]', ' ', text_description)
